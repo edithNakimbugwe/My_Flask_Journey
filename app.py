cab9adb3c96d5ea386f9ecda7ee975ee3181abab
Edith Nakimbugwe <shanitadith@gmail.com>
2025-01-15 13:07:06 +0300

    Initial Commit

---
+++ app.py
@@ -0,0 +1,14 @@
+#imports
+from flask import Flask, render_template
+from flask_scss import Scss
+from flask_sqlalchemy import SQLAlchemy
+
+#my app
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ in "__main__":
+    app.run(debug=True)
